25f70dd69ba8486bb597a37c1483dee525b26f2e
elsewise <airywp@qq.com>
2016-07-28 16:59:38 +0800

    init

--- src/Demo/JTest/JTest.php
+++ src/Demo/JTest/JTest.php
@@ -1,12 +1,14 @@
 <?php
 
+namespace Demo\JTest;
+
 /**
  * Author: Elsewise Airy @wp
  * E-mail: airywp@qq.com
  * Date: 2016/7/28
  * Time: 14:50
  */
-class Test {
+class JTest {
 	public function hello() {
 		echo "Hello,world";
 	}

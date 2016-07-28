27f600b50c31400e52da08364b0b9ebee60f7112
elsewise <airywp@qq.com>
2016-07-28 15:49:09 +0800

    init

---
+++ src/Demo/JTest/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Author: Elsewise Airy @wp
+ * E-mail: airywp@qq.com
+ * Date: 2016/7/28
+ * Time: 14:50
+ */
+class Test {
+	public function hello() {
+		echo "Hello,world";
+	}
+}
